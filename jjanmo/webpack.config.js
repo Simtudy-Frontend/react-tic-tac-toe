4ac99a742a2ae3ff13632e381baacc2d1c328eb5
jjanmo <jjanmo07@hanmail.net>
2024-09-04 03:13:43 +0900

    feat: 보일러러플레이트 없이 리액트+ 웹팩 프로젝트 생성 및 웹팩 설정

---
+++ jjanmo/webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          target: 'es2015',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      favicon: './public/favicon.png',
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 9000,
+    hot: true,
+  },
+}
