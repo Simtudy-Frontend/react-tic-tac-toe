887ca0ce6370c29211cd273a6ef9fbeec119c7cc
jjanmo <jjanmo07@hanmail.net>
2024-09-04 22:01:35 +0900

    fix: 절대경로를 위한 설정 추가

--- jjanmo/webpack.config.js
+++ jjanmo/webpack.config.js
@@ -32,6 +32,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   devServer: {
     static: {
