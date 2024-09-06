5ada3d7cedaa8a84cea39938d0add6631d2ab814
김현진 <guswls1846@naver.com>
2024-09-06 17:25:07 +0900

    refactor: Move Board component to @/game/components

--- hyunjin/src/game/index.jsx
+++ hyunjin/src/game/index.jsx
@@ -1,50 +1,5 @@
 import { useState } from "react";
-import { calculateWinner } from "@/game/utils";
-
-
-const Board = ({ xIsNext, squares, onPlay }) => {
-  const handleClick = (i) => {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-    onPlay(nextSquares);
-  };
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
-  return (
-    <div>
-      <div className="mb-2.5">{status}</div>
-      <div className="clearfix after:content-[''] after:table after:clear-both">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="clearfix after:content-[''] after:table after:clear-both">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="clearfix after:content-[''] after:table after:clear-both">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </div>
-  );
-};
+import { Board } from "./components";
 
 export const Game = () => {
   const [history, setHistory] = useState([Array(9).fill(null)]);
