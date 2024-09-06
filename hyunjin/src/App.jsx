e18cb9a7a02e16e70622658c7464ff22d2fb5e49
김현진 <guswls1846@naver.com>
2024-09-06 16:00:00 +0900

    chore: Update Tailwind CSS configuration and styles

--- hyunjin/src/App.jsx
+++ hyunjin/src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <>Welcome to HyunJin Tic Tac Toe!</>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Welcome to HyunJin Tic Tac Toe!
+    </h1>
+  );
 }
 
 export default App;
