cb45c13932a7dcef0b28951fc19f6373cdc72f65
kwonah0 <42600987+kwonah0@users.noreply.github.com>
2024-09-08 22:23:19 +0900

    add gameUtil

---
+++ ayoung/src/utils/gameUtils.js
@@ -0,0 +1,45 @@
+const lines = [
+    // 가로
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // 세로
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //대각선
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const calcGameStatus = (squares) => {
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return {
+                ended: true,
+                winner: squares[a]
+            };
+        };
+    }
+    if (squares.every((cell) => cell !== null)) {
+        return {
+            ended: true,
+            winner: null
+        };
+    }
+    return {
+        ended: false,
+        winner: null
+    };
+};
+
+const getComputerPosition = (squares) => {
+    // 빈 자리 랜덤 선택
+    const availableSquares = squares
+        .map((sq, index) => (sq === null ? index : null))
+        .filter((index) => index !== null);
+    return availableSquares[Math.floor(Math.random() * availableSquares.length)];
+};
+
+export { calcGameStatus, getComputerPosition };
