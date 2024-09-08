cc2833778a4ce72129708206bb22d14a0e048ed5
kwonah0 <42600987+kwonah0@users.noreply.github.com>
2024-09-08 22:24:08 +0900

    player vs computer w/ player selection

--- ayoung/src/App.jsx
+++ ayoung/src/App.jsx
@@ -1,10 +1,50 @@
+import { useState, useEffect } from "react";
+import SelectPlayer from "@/components/SelectPlayer";
+import Board from "@/components/Board";
+import { calcGameStatus, getComputerPosition } from "@/utils/gameUtils";
 import styled from "styled-components";
-import Board from "./Board";
 
 const App = () => {
+  const [playerMark, setPlayerMark] = useState("X");
+  const [mark, setMark] = useState("X");
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const isPlayerTurn = playerMark === mark;
+  const gameStatus = calcGameStatus(squares);
+
+  const handlePlayerSelect = (selectedMark) => {
+    if (playerMark === selectedMark || gameStatus.ended) {
+      return;
+    }
+    setPlayerMark(selectedMark);
+  };
+
+  const handleClick = (idx) => {
+    if (!isPlayerTurn || squares[idx] || gameStatus.ended) return;
+    updateSquares(idx, mark);
+  };
+
+  const updateSquares = (idx, currentMark) => {
+    const newSquares = [...squares];
+    newSquares[idx] = currentMark;
+    setSquares(newSquares);
+    setMark((prevMark) => (prevMark === "X" ? "O" : "X"));
+  };
+
+  useEffect(() => {
+    if (isPlayerTurn || gameStatus.ended) return;
+    const position = getComputerPosition(squares, mark);
+    updateSquares(position, mark);
+  }, [isPlayerTurn, mark, squares]);
+
   return (
     <Container>
-      <Board></Board>
+      <SelectPlayer
+        player={playerMark}
+        onPlayerSelect={handlePlayerSelect}
+        turnMark={gameStatus.ended ? null : mark}
+      ></SelectPlayer>
+      <Board squares={squares} onBoardClick={handleClick}></Board>
     </Container>
   );
 };
